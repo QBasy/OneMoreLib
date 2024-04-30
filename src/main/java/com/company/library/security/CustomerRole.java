068940b5eeae6272ac722c908280ac7848831cb7
QoidynBasy <c0mplexie1@gmail.com>
2024-04-30 12:12:59 +0600

    oneMoreLib

--- src/main/java/com/company/library/security/CustomerRole.java
+++ src/main/java/com/company/library/security/CustomerRole.java
@@ -27,7 +27,7 @@ public interface CustomerRole {
     @EntityPolicy(entityClass = Book.class, actions = EntityPolicyAction.READ)
     void book();
 
-    @MenuPolicy(menuIds = {"BookHistory.list", "Book.list", "datatl_entityInspectorListView"})
-    @ViewPolicy(viewIds = {"BookHistory.list", "Book.list", "LoginView", "MainView", "BookHistory.detail", "datatl_entityInspectorListView", "entityInfoView", "datatl_entityInspectorDetailView", "flowui_AddConditionView", "flowui_GroupFilterCondition.detail", "flowui_JpqlFilterCondition.detail", "flowui_PropertyFilterCondition.detail", "changePasswordView"})
+    @MenuPolicy(menuIds = {"BookHistory.list", "Book.list", "UserBook.list"})
+    @ViewPolicy(viewIds = {"BookHistory.list", "Book.list", "LoginView", "MainView", "BookHistory.detail", "entityInfoView", "flowui_AddConditionView", "flowui_GroupFilterCondition.detail", "flowui_JpqlFilterCondition.detail", "flowui_PropertyFilterCondition.detail", "changePasswordView", "datatl_entityInspectorListView", "datatl_entityInspectorDetailView", "inputDialog", "UserBook.list"})
     void screens();
 }
